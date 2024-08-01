d0372f48751274c0d6565c9df2abd8b51a5c2414
Devon <o_devon2@yahoo.com>
2024-08-01 16:18:12 -0400

    Finished using backtracking

--- main.py
+++ main.py
@@ -9,6 +9,42 @@ board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]]
 
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, [row, col]):
+            bo[row][col] = i
+            
+            if solve(bo):
+                return True
+            bo[row][col] = 0
+    return False
+
+
+
+
+def valid(bo, num, pos):
+    #Check row
+    if bo[pos[0]].count(num) > 0:
+        return False
+    #Check Coloumn
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    #Check Box
+    box_X = pos[1] // 3    
+    box_Y = pos[0] // 3
+    for i in range(box_Y * 3, box_Y * 3 + 3):
+        for j in range(box_X * 3, box_X * 3 + 3):
+            if bo[i][j] == num and (pos[0] != i and pos[1] != j):
+                return False
+    return True
+
+#Prints the board to terminal 
 def print_board(bo):
     for i in range(len(bo)):
         if not i % 3 and i !=0:
@@ -22,4 +58,17 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
+#Finds the first empty cell in a board, from left to right, top down
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return [i,j]
+    return None
+
+
+
+print_board(board)
+solve(board)
+print("\n")
 print_board(board)
